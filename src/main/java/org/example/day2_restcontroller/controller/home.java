6014514421595dd7b8f50433c4612084e3ac7387
tushar <tusharbansal3260@gmail.com>
2026-01-19 15:07:47 +0530

    added first api

--- src/main/java/org/example/day2_restcontroller/controller/home.java
+++ src/main/java/org/example/day2_restcontroller/controller/home.java
@@ -1,12 +1,15 @@
 package org.example.day2_restcontroller.controller;
 
+import org.example.day2_restcontroller.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class home {
-    @GetMapping("/about")
-    public String Home(){
-        return "This is about page";
+    @GetMapping("/")
+    public StudentModel getStudent(){
+        StudentModel student = new StudentModel(1,"Tushar","tusharbansal@gmail.com");
+        return student;
+
     }
 }
